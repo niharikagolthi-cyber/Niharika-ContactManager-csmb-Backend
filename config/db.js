6915a396cd9f67dc0d933992775ebce981dd9e12
niharikagolthi-cyber <niharikagolthi@gmail.com>
2026-07-14 16:19:33 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dns = require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://adariadithya816_db_user:kzsMeiCBrVfg2dUR@cluster0.qfimjmp.mongodb.net/Contactmanager");
+        await mongoose.connect("mongodb+srv://niharikagolthi:niharika@cluster0.0ys5bsa.mongodb.net/contacts");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
